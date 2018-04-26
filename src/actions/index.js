e32a9a002ca8960c61c13187e0d656c33c33b751
Michal Turek <michael.turek@rizk.com>
2018-04-26 14:33:28 +0200

    revert-commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API_KEY = 'a848489120fc64af00163991228fe8a6';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 export const FETCH_WEATHER_ERROR = 'FETCH_WEATHER_ERROR';
